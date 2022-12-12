b8a3df0405e2d072029ddba8498d4cb5ec362c52
DinkyWinky <120288467+DinkyWinky7@users.noreply.github.com>
2022-12-12 13:47:23 -0600

    Create HeadManager.cs
    created the main manager for the game

---
+++ HeadManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace HeadManager
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            
+            Console.Read();
+        }
+    }
+}
