66fdebc9fbedbf45657e6925dfd9dcd46caf7244
DinkyWinky <120288467+DinkyWinky7@users.noreply.github.com>
2022-12-14 19:55:19 +0000

    Made Player.cs, GameManager.cs, and Overworld.cs

---
+++ Data/Player.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace Player
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int power { get; set; }
+        public int health { get; set; }
+        public float experience { get; set; }
+        public int level { get; set; }
+    }
+}
---
+++ Game/GameManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace GameManager
+{
+    public class GameManagerProgram
+    {
+        public void Manager()
+        {
+            
+        }
+    }
+}
